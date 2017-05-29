7ae65e5d556f2e95fec1ffc590aa9a45c189c5c6
Lewis Nixon <lewcfn@gmail.com>
2017-05-29 17:33:15 +0100

    install request and save  ** fixup **

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const corsOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/account.js
+++ routes/account.js
@@ -5,42 +5,24 @@ const router  = express.Router();
 const apiKey = process.env.API_KEY;
 const apiSecret = process.env.API_SECRET;
 const accountID = process.env.ACCOUNT_ID;
+const AccountClient = require('../api/account');
+const requestConfig = {
+  method: 'GET',
+  path: `/v2/accounts/${accountID}`,
+  body: ''
+};
+const accountClient = new AccountClient(requestConfig, crypto, apiKey, apiSecret, accountID, request);
 
 router.get('/', (req, res, next) => {
-  // set the parameter for the request message
-  const requestConfig = {
-    method: 'GET',
-    path: `/v2/accounts/${accountID}`,
-    body: ''
-  };
-  const timestamp = Math.floor(Date.now() / 1000);
-  const message = timestamp + requestConfig.method + requestConfig.path + requestConfig.body;
-
-  //create a hexedecimal encoded SHA256 signature of the message
-  const signature = crypto.createHmac('sha256', apiSecret).update(message).digest('hex');
-
-  const options = {
-    baseUrl: 'https://api.coinbase.com/',
-    url: requestConfig.path,
-    method: requestConfig.method,
-    headers: {
-      'CB-ACCESS-SIGN': signature,
-      'CB-ACCESS-TIMESTAMP': timestamp,
-      'CB-ACCESS-KEY': apiKey,
-      'CB-VERSION': '2016-04-30'
-    }
-  };
-
-  console.log('getting accounts...');
-  request(options, (err, response) => {
-
+  accountClient.getAccount((err, response) => {
     if (err) {
       console.log(err);
     }
-    console.log('sonding...\n', response.body);
+
     res.send(response.body);
 
   });
+
 });
 
 module.exports = router;
